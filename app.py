a58ac7a4d76f6035174962f1aaa48f235e2b3dd3
12boris <bi_igdal@mail.ru>
2023-05-01 19:12:52 +0300

    first

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ db = SQLAlchemy(app)
 
 class entryes(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(300), nullable=False)
     entry = db.Column(db.String(300), nullable=False)
 
     def __repr__(self):
-        return '<entry %r>' % self.name
+        return '<entry %r>' % self.entry
     
 
 # так как скрипт предназначен для одного человека, 
@@ -22,51 +21,29 @@ class entryes(db.Model):
 @app.route('/entry', methods=['GET', 'POST'])
 def my_entry():
     if request.method == 'POST':
-        # удаление записи
-        try:
-            entry_id = request.form['deleted_entry']
-            entry = entryes.query.filter_by(id=entry_id).first()
-            db.session.delete(entry)
-            db.session.commit()
-        except:
-            pass
 
         # список записей
-        entryes_list = entryes.query.all()
-        return render_template('bots.html', entryes_list=entryes_list)
+        content_entry = request.form['entry']
+        entry = entryes.query.first()
+        entry.entry = content_entry
+        db.session.commit()
+
+        print(entry)
+
+        return render_template('bots.html', entry=entry)
     
     else:
         # список записей
-        entryes_list = entryes.query.all()
-        return render_template('bots.html', entryes_list=entryes_list)
-    
-
-# добавление записи
-@app.route('/entry-add', methods=['GET', 'POST'])
-def add_entry():
-    if request.method == 'POST':
-        # создание записи
-        try:
-            insert_entry = request.form['entry']
-            name = insert_entry[:10]
-            entry = entryes(name=name, entry=insert_entry)
-            db.session.add(entry)
-            db.session.commit()
-        except:
-            pass
-
-        # список записей
-        entryes_list = entryes.query.all()
-        return render_template('bots.html', entryes_list=entryes_list)
-    
-    else:
-        # список записей
-        entryes_list = entryes.query.all()
-        return render_template('bots.html', entryes_list=entryes_list)
+        entry = entryes.query.first()
+        return render_template('bots.html', entry=entry)
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        if not entryes.query.first():
+            first_entry = entryes(entry="")
+            db.session.add(first_entry)
+            db.session.commit()
         
     app.run(debug=False, port=5050)
